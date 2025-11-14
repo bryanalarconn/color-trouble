8e2a879cb7dc7ff8f32fa0d011908cb55b6d02ab
Bryan Alarcon <bryanosminalarcon@gmail.com>
2025-11-13 19:31:57 -0800

    added live webcam feed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+  import React from 'react';
+  import Webcam from 'react-webcam';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const WebcamDisplay = () => {
+    const videoConstraints = {
+      facingMode: "selfie" // "user" for front camera, "environment" for rear camera
+    };
 
-export default App;
+    return (
+      <div>
+        <h2>Live Webcam Feed</h2>
+        <Webcam
+          height={480}
+          width={640}
+          videoConstraints={videoConstraints}
+        />
+      </div>
+    );
+  };
+
+  export default WebcamDisplay;
