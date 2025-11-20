86cb3f079aa3756f7b99c06b627671622b300f21
Bryan Alarcon <bryanosminalarcon@gmail.com>
2025-11-19 21:06:30 -0800

    added video constraints

--- src/WebcamImage.js
+++ src/WebcamImage.js
@@ -1,6 +1,12 @@
 import React, { useCallback, useRef } from "react";
 import Webcam from "react-webcam";
 
+const videoConstraints = {
+  width: { ideal: 1280 },
+  height: { ideal: 720 },
+  facingMode: "user",
+};
+
 // Webcam component to capture images from the user's webcam
 function WebcamImage({ onCapture }) {
   // Reference to the webcam component
@@ -15,15 +21,21 @@ function WebcamImage({ onCapture }) {
     }
   }, [onCapture]);
   return (
-  //will call this classname in the css
-  <div className="webcam-wrapper">
-    <div className="camera-box">
-      <Webcam mirrored={true} ref={webcamRef} screenshotFormat="image/jpeg" className="camera-feed"/>
+    //will call this classname in the css
+    <div className="webcam-wrapper">
+      <div className="camera-box">
+        <Webcam
+          mirrored={true}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          className="camera-feed"
+          videoConstraints={videoConstraints}
+        />
+      </div>
+      <button className="capture-btn" onClick={capture}>
+        Capture Colors
+      </button>
     </div>
-    <button className="capture-btn" onClick={capture}>
-      Capture Colors
-    </button>
-  </div>
   );
 }
 
